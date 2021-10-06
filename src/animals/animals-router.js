0953378796a25ce3fe40de368a63e49f47ba5932
Christine Yoo <christinecho.s@gmail.com>
2021-10-05 20:19:19 -0700

    remove semicolons

--- src/animals/animals-router.js
+++ src/animals/animals-router.js
@@ -1,49 +1,49 @@
-const express = require('express');
-const path = require('path');
-const AnimalsService = require('./animals-service');
-const xss = require('xss');
+const express = require('express')
+const path = require('path')
+const AnimalsService = require('./animals-service')
+const xss = require('xss')
 
-const animalsRouter = express.Router();
-const jsonParser = express.json();
+const animalsRouter = express.Router()
+const jsonParser = express.json()
 
 const serializeAnimal = (animal) => ({
   animal_id: animal.animal_id,
   animal_type: xss(animal.animal_type),
   animal_description: xss(animal.animal_description),
   belongs_to_pid: xss(animal.belongs_to_pid)
-});
+})
 
 animalsRouter
   .route('/')
   .get((req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get('db')
     AnimalsService.getAllAnimals(knexInstance)
       .then((animals) => {
-        res.json(animals.map(serializeAnimal));
+        res.json(animals.map(serializeAnimal))
       })
       .catch((err) => {
-        console.log({ err });
-        next();
-      });
+        console.log({ err })
+        next()
+      })
   })
   .post(jsonParser, (req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get('db')
     const {
       animal_type,
       animal_description,
       belongs_to_pid
-    } = req.body;
+    } = req.body
     const newAnimal = {
         animal_type,
         animal_description,
         belongs_to_pid
-    };
+    }
 
     for (const [key, value] of Object.entries(newAnimal)) {
       if (value == null) {
         return res.status(400).json({
           error: { message: `Missing '${key}' in request body` }
-        });
+        })
       }
     }
 
@@ -52,68 +52,68 @@ animalsRouter
         return res
           .status(201)
           .location(path.posix.join(req.originalUrl, `/${animal.animal_id}`))
-          .json(serializeAnimal(animal));
+          .json(serializeAnimal(animal))
       })
       .catch((err) => {
-        console.log({ err });
-        next();
-      });
-  });
+        console.log({ err })
+        next()
+      })
+  })
 
 animalsRouter
   .route('/:animal_id')
   .all((req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get('db')
     AnimalsService.getById(knexInstance, req.params.animal_id)
       .then((animal) => {
         if (!animal) {
           return res
             .status(404)
-            .json({ error: { message: `Animal doesn't exist` } });
+            .json({ error: { message: `Animal doesn't exist` } })
         }
-        res.animal = animal;
-        next();
+        res.animal = animal
+        next()
       })
       .catch((err) => {
-        console.log({ err });
-        next();
-      });
+        console.log({ err })
+        next()
+      })
   })
   .get((req, res, next) => {
-    res.json(serializeAnimal(res.animal));
+    res.json(serializeAnimal(res.animal))
   })
   .delete((req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get('db')
     AnimalsService.deleteAnimal(knexInstance, req.params.animal_id)
       .then((numRowsAffected) => {
-        res.json({ message: `Successfully deleted` });
-        res.status(204).end();
+        res.json({ message: `Successfully deleted` })
+        res.status(204).end()
       })
       .catch((err) => {
-        console.log({ err });
-        next();
-      });
+        console.log({ err })
+        next()
+      })
   })
   .patch(jsonParser, (req, res, next) => {
-    const knexInstance = req.app.get('db');
+    const knexInstance = req.app.get('db')
     const {
         animal_type,
         animal_description,
         belongs_to_pid
-    } = req.body;
+    } = req.body
     const animalToUpdate = {
         animal_type,
         animal_description,
         belongs_to_pid
-    };
+    }
 
-    const numberOfValues = Object.values(animalToUpdate).filter(Boolean).length;
+    const numberOfValues = Object.values(animalToUpdate).filter(Boolean).length
     if (numberOfValues === 0)
       return res.status(400).json({
         error: {
           message: `Request body must contain 'animal_type', 'animal_description', or 'belongs_to_pid'.`
         }
-      });
+      })
 
     AnimalsService.updateAnimal(
       knexInstance,
@@ -121,13 +121,13 @@ animalsRouter
       animalToUpdate
     )
       .then((numRowsAffected) => {
-        res.json({ message: `Successfully updated` });
-        res.status(204).end();
+        res.json({ message: `Successfully updated` })
+        res.status(204).end()
       })
       .catch((err) => {
-        console.log({ err });
-        next();
-      });
-  });
+        console.log({ err })
+        next()
+      })
+  })
 
-module.exports = animalsRouter;
+module.exports = animalsRouter
--- src/people/people-service.js
+++ src/people/people-service.js
@@ -1,24 +1,24 @@
 const PeopleService = {
     getAllPeople(knex) {
-      return knex('people').select('*');
+      return knex('people').select('*')
     },
     insertPerson(knex, newPerson) {
       return knex('people')
         .insert(newPerson)
         .returning('*')
         .then((rows) => {
-          return rows[0];
-        });
+          return rows[0]
+        })
     },
     getById(knex, id) {
-      return knex('people').select('*').where({ person_id : id }).first();
+      return knex('people').select('*').where({ person_id : id }).first()
     },
     deletePerson(knex, id) {
-      return knex('people').where({ person_id : id }).delete();
+      return knex('people').where({ person_id : id }).delete()
     },
     updatePerson(knex, id, newPersonFields) {
-      return knex('people').where({ person_id : id }).update(newPersonFields);
+      return knex('people').where({ person_id : id }).update(newPersonFields)
     }
-  };
+  }
   
-  module.exports = PeopleService;
+  module.exports = PeopleService
